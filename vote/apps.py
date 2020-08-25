a4d1e19dd39e75df97bee53420a38707dd426a07
Ervin Marguč <ervin.marguc@kazoja.si>
2020-08-25 08:30:26 +0200

    models

--- vote/apps.py
+++ vote/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig as BaseConfig
-from django.utils.translation import gettext as _
+#from django.utils.translation import gettext as _
+from django.utils.translation import ugettext_lazy as _
 
 
 class VoteAppConfig(BaseConfig):
--- vote/templatetags/vote.py
+++ vote/templatetags/vote.py
@@ -3,13 +3,13 @@ from __future__ import absolute_import
 from django import template
 from django.contrib.auth.models import AnonymousUser
 
-from vote.models import UP
+from vote.models import Vote  # UP
 
 register = template.Library()
 
 
 @register.simple_tag
-def vote_exists(model, user=AnonymousUser(), action=UP):
+def vote_exists(model, user=AnonymousUser(), action=Vote.UP):
     if user.is_anonymous:
         return False
     return model.votes.exists(user.pk, action=action)
