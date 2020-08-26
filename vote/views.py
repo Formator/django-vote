888428a93fc36f8cbdc64cde62a07946e0040938
Ervin Marguč <ervin.marguc@kazoja.si>
2020-08-26 14:53:35 +0200

    instance

--- vote/views.py
+++ vote/views.py
@@ -1,6 +1,6 @@
+from rest_framework import generics, mixins, status
 from rest_framework.decorators import action
 from rest_framework.views import Response
-from rest_framework import mixins, generics, status
 
 from vote.signals import post_voted
 
@@ -27,9 +27,8 @@ class CreateChangeDeleteVoteAPIView(mixins.CreateModelMixin,
         else:
             return Response(data={}, status=409)
 
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        #self.perform_create(serializer)
+        serializer = self.get_serializer(
+            instance=getattr(obj.votes, 'instance'))
         headers = self.get_success_headers(serializer.data)
         return Response(
             serializer.data, status=status.HTTP_201_CREATED, headers=headers)
